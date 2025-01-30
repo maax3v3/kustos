78778ab5c7515ba4ea6a5ff75d6212f62ee629d7
Maxence Lequeux <maxence.lequeux@lsmart.co>
2025-01-30 17:07:37 +0100

    Sidebar catalog

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -3,15 +3,72 @@ import {
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
     SidebarHeader,
-} from "@/components/ui/sidebar"
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarMenuSubButton,
+    SidebarMenuSubItem,
+} from "@/components/ui/sidebar";
+import { getAllTags } from "@/lib/queries/get-all-tags";
+import { TagsList } from "@/types/tags-list";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./ui/collapsible";
+import { ChevronRight } from "lucide-react";
 
 export default function AppSidebar() {
+    const [tagsLists, setTagsLists] = useState<TagsList[]>([]);
+
+    useQuery({
+        queryKey: ['tags-lists'],
+        queryFn: () => getAllTags(),
+        onSuccess(data) {
+            setTagsLists(data);
+        },
+    })
+
     return (
         <Sidebar>
             <SidebarHeader />
             <SidebarContent>
-                <SidebarGroup />
+                <SidebarGroup>
+                    <SidebarGroupLabel>Catalog</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {tagsLists.map((tagsList) => (
+                                <Collapsible
+                                    key={tagsList.name}
+                                    asChild
+                                    className="group/collapsible"
+                                >
+                                    <SidebarMenuItem key={tagsList.name}>
+                                        <CollapsibleTrigger asChild>
+                                            <SidebarMenuButton>
+                                                <span>{tagsList.name}</span>
+                                                <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                                            </SidebarMenuButton>
+                                        </CollapsibleTrigger>
+                                        <CollapsibleContent>
+                                            <SidebarMenuSub>
+                                                {tagsList.tags.map(tag => (
+                                                    <SidebarMenuSubItem key={tag}>
+                                                        <SidebarMenuSubButton>
+                                                            <span>{tag}</span>
+                                                        </SidebarMenuSubButton>
+                                                    </SidebarMenuSubItem>
+                                                ))}
+                                            </SidebarMenuSub>
+                                        </CollapsibleContent>
+                                    </SidebarMenuItem>
+                                </Collapsible>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
             </SidebarContent>
             <SidebarFooter />
         </Sidebar>
